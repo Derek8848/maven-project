8428c145e799b5b25f34adf1af6f499696d95bcc
Derek Ding <dingker@gmail.com>
2019-03-05 11:06:39 +0800

    update comments

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,10 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+/**
+ * This is a update comments
+ */
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
