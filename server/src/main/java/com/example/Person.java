bf66771c303486160b539fd5b4cb510ae2585527
Derek Ding <dingker@gmail.com>
2019-03-05 17:28:03 +0800

    update in vscode

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -13,7 +13,7 @@ public class Person {
   }
 
 /**
- * This is a update comments
+ * This is a update comments ....
  */
   public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
